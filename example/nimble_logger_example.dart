fa605ed22989625046d941695c79e64bfd5cc9b1
David J. Fink <david@darkroastcreative.co>
2022-02-16 19:07:55 -0700

    Initial Commit

---
+++ example/nimble_logger_example.dart
@@ -0,0 +1,6 @@
+import 'package:nimble_logger/nimble_logger.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/nimble_logger.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library nimble_logger;
+
+export 'src/nimble_logger_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
