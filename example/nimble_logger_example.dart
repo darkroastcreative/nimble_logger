44af0539c21b3de1ed1475dbb70144fb0fa87c15
David J. Fink <david@darkroastcreative.co>
2023-02-05 20:16:14 -0700

    Added Call to logError() in Package Usage Example, Comments to Explain Example

--- example/nimble_logger_example.dart
+++ example/nimble_logger_example.dart
@@ -2,9 +2,17 @@ import 'package:nimble_logger/nimble_logger.dart';
 import 'package:nimble_logger/src/utility_functions.dart';
 
 void main() {
+  // Declare and initialize an instance of NimbleLogger with console logging
+  // enabled.
   NimbleLogger logger = NimbleLogger([LoggingMethod.console]);
+
+  // Set path for file-based logging.
   logger.setFileLoggingPath(
       '/Users/david/Documents/Logs/TestLog ${getSanitizedTimestampString(DateTime.now())}.log');
+  // Enable file-based logging.
   logger.setFileLoggingState(true);
+
+  // Log a couple test messages to console and file.
   logger.logMessage('Hello, world!');
+  logger.logError('On no!');
 }
